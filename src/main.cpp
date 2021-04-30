49e9d71694cff07c91630f6fc64dfa098589e5c2
Jacob Tracey <42984369+Jacob-Tr@users.noreply.github.com>
2021-04-30 19:01:02 -0300

    Add files via upload
    - Various bug fixes.
    - Ability to check if a folder exists.
    - Ability to remove existing folders.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #ifndef WIN32
 #include <unistd.h>
 #include <sys\stat.h>
-#define _malloca malloc
-#define _mkdir mkdir
+
+#define _malloca malloc // SA-MP plugin SDK breaks without this.
 #else
 #include "Headers\Unistd.h"
 #endif
@@ -15,32 +15,95 @@
 
 char dir[LOCALE_NAME_MAX_LENGTH + 5] = "";
 
-cell AMX_NATIVE_CALL MkDir(AMX* amx, cell* params)
+int DoesFileExist(char* dir)
 {
-    if(strlen(dir) == 0) return 0;
+    int success = 0;
+#ifdef WIN32
+    success = _mkdir(dir);
+#else
+    success = mkdir(dir, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+#endif
+
+    if(success == -1) return 1;
+#ifdef WIN32
+    _rmdir(dir);
+#else
+    rmdir(dir);
+#endif
+    return 0;
+}
+
+void GetDirectory(const char* string, char* dest)
+{
     char new_dir[LOCALE_NAME_MAX_LENGTH];
-
-    char* string = nullptr;
-
-    amx_StrParam(amx, params[1], string);
-
-    if(!IsValidFolderName(string)) return 0;
-
 #ifdef WIN32
     snprintf(new_dir, LOCALE_NAME_MAX_LENGTH, "%s\\%s", dir, string);
 #else
     snprintf(new_dir, LOCALE_NAME_MAX_LENGTH, "%s/%s", dir, string);
 #endif
 
-    char str[128] = "";
-    snprintf(str, 128, "<Plugin> File Created: scriptfiles/%s", new_dir);
+    dest = new_dir;
+}
 
-    logprintf(str);
+cell AMX_NATIVE_CALL RmDir(AMX* amx, cell* params)
+{
+	char* string = nullptr;
+
+	amx_StrParam(amx, params[1], string);
+
+	char dir[LOCALE_NAME_MAX_LENGTH];
+	GetDirectory(string, dir);
+
+	if(!DoesFileExist(dir)) return 0;
+	
+	char str[128] = "";
+	snprintf(str, 128, "<Plugin> Folder Deleted: scriptfiles/%s", string);
+	
+#ifdef WIN32
+	_rmdir(dir);
+#else
+	rmdir(dir);
+#endif
+	
+	logprintf(str);
+	return 1;
+}
+
+cell AMX_NATIVE_CALL ChDir(AMX* amx, cell* params)
+{
+    char* string = nullptr;
+
+    amx_StrParam(amx, params[1], string);
+
+    char dir[LOCALE_NAME_MAX_LENGTH];
+    GetDirectory(string, dir);
+
+    return DoesFileExist(dir);
+}
+
+cell AMX_NATIVE_CALL MkDir(AMX* amx, cell* params)
+{
+    char* string = nullptr;
+
+    amx_StrParam(amx, params[1], string);
+
+    if(!IsValidFolderName(string)) return 0;
+    
+	char new_dir[LOCALE_NAME_MAX_LENGTH];
+    GetDirectory(string, new_dir);
+
+    if(DoesFileExist(new_dir)) return 0;
+
+    char str[128] = "";
+    snprintf(str, 128, "<Plugin> Folder Created: scriptfiles/%s", string);
+	
 #ifdef WIN32
     _mkdir(new_dir);
 #else
-	_mkdir(new_dir, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+	mkdir(new_dir, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 #endif
+	
+	logprintf(str);
     return 1;
 }
 
@@ -54,23 +117,21 @@ PLUGIN_EXPORT int PLUGIN_CALL Load(void **ppData)
     snprintf(dir, LOCALE_NAME_MAX_LENGTH, "%s", getcwd(dir, LOCALE_NAME_MAX_LENGTH));
 
     char folder_dir[128];
-    snprintf(folder_dir, LOCALE_NAME_MAX_LENGTH, "<Plugin> loaded at: %s", dir);
+    snprintf(folder_dir, (LOCALE_NAME_MAX_LENGTH + 20), "<Plugin> Loaded at: %s", dir);
 
-#ifdef WIN32
-    snprintf(dir, LOCALE_NAME_MAX_LENGTH, "%s\\%s", dir, "scriptfiles");
-#else
-    snprintf(dir, LOCALE_NAME_MAX_LENGTH, "%s/%s", dir, "scriptfiles");
-#endif
+    GetDirectory("scriptfiles", dir);
 
     logprintf(folder_dir);
     return 1;
 }
 
-PLUGIN_EXPORT void PLUGIN_CALL Unload() {logprintf("<Plugin> unloaded.");}
+PLUGIN_EXPORT void PLUGIN_CALL Unload() {logprintf("<Plugin> Unloaded.");}
 
 AMX_NATIVE_INFO PluginNatives[] =
 {
     {"MkDir", MkDir},
+    {"ChDir", ChDir},
+    {"RmDir", RmDir},
     {0, 0}
 };
 
